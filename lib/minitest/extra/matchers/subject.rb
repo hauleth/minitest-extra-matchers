dc51f717e7bc34d09992e06862aab03c445b9562
Łukasz Niemier <lukasz@niemier.pl>
2014-02-04 12:35:01 +0100

    Fix module errors

--- lib/minitest/extra/matchers/subject.rb
+++ lib/minitest/extra/matchers/subject.rb
@@ -1,37 +1,43 @@
-module Minitest::Extra::Matchers::Subject
-  def self.included(base)
-    base.extend ClassMethods
-    base.class_eval do
-      alias_method :context, :describe
-    end
-  end
+module Minitest
+  module Extra
+    module Matchers
+      module Subject
+        def self.included(base)
+          base.extend ClassMethods
+          base.class_eval do
+            alias_method :context, :describe
+          end
+        end
 
-  Minitest::Expectations.methods.grep(/^(must|wont)_/).each do |m|
-    define_method m do |*args, &block|
-      subject.send(m, *args, &block)
-    end
-  end
+        Minitest::Expectations.methods.grep(/^(must|wont)_/).each do |m|
+          define_method m do |*args, &block|
+            subject.send(m, *args, &block)
+          end
+        end
 
-  def pending
-    skip('Not yet implemented...')
-  end
+        def pending
+          skip('Not yet implemented...')
+        end
 
-  module ClassMethods
-    def subject(name = nil, &block)
-      if name
-        let(name, &block)
-        let(:subject) { send(name) }
-      else
-        let(:subject, &block)
+        module ClassMethods
+          def subject(name = nil, &block)
+            if name
+              let(name, &block)
+              let(:subject) { send(name) }
+            else
+              let(:subject, &block)
+            end
+          end
+
+          def its(name, &block)
+            it { subject.send(name).instance_eval(&block) }
+          end
+
+          def pending
+            it { pending }
+          end
+        end
       end
-    end
-
-    def its(name, &block)
-      it { subject.send(name).instance_eval(&block) }
-    end
-
-    def pending
-      it { pending }
     end
   end
 end
--- lib/minitest/extra/matchers/version.rb
+++ lib/minitest/extra/matchers/version.rb
@@ -1,7 +1,7 @@
 module Minitest
   module Extra
     module Matchers
-      VERSION = "0.0.2"
+      VERSION = "0.0.3"
     end
   end
 end
