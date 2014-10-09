de8d284c8c47b7fed0baaf0f9cba7bfb1f36f0ee
Łukasz Niemier <lukasz@niemier.pl>
2014-10-09 21:01:29 +0200

    Add attribute matcher

--- lib/minitest/extra/matchers/dom.rb
+++ lib/minitest/extra/matchers/dom.rb
@@ -5,14 +5,22 @@ module Minitest
         def self.included(base)
           base.class_eval do
             infect_an_assertion :assert_dom_equal, :must_equal_dom
+            infect_an_assertion :assert_attribute_equal, :must_have_attribute,
+                                :dont_flip
           end
         end
 
         def assert_dom_equal(expected, actual)
-          expected_dom = HTML::Document.new(expected).root
-          actual_dom   = HTML::Document.new(actual).root
+          expected_dom = Nokogiri::HTML(expected).root
+          actual_dom   = Nokogiri::HTML(actual).root
 
           assert_equal expected_dom, actual_dom
+        end
+
+        def assert_attribute_equal(element, name, value)
+          actual = HTML::Document.new(element).root.attribute(name)
+
+          assert_equal value, actual
         end
       end
     end
